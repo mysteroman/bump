d790b09f5ffc05a4af1adf99285858e956b7d116
mysteroman <mysteroman.minecraft@gmail.com>
2022-12-15 11:59:59 -0500

    fix api

--- app/Models/Validators/QueryValidator.php
+++ app/Models/Validators/QueryValidator.php
@@ -6,15 +6,16 @@ class QueryValidator
 {
     public static function validate($form): bool
     {
+        $form->field('route')->validate([
+            Rule::notEmpty(),
+            Rule::maxLength(255)
+        ]);
         $form->field('placeId')->validate([
             Rule::notEmpty(),
             CustomRule::placeId(),
             Rule::maxLength(255)
         ], true);
-        $form->field('route')->validate([
-            Rule::notEmpty(),
-            Rule::maxLength(255)
-        ]);
+        
         return $form->verify();
     }
 }
